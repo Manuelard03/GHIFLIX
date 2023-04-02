93f88a5a20d4f32de923fd0c3445767753889e5d
Manuela <manuelarodriguezdavila@outlook.com>
2023-04-02 17:55:52 -0500

    modificaciones landing page

---
+++ 1. LANDING PAGE/script-landing.js
@@ -0,0 +1,12 @@
+const togglebtn = document.querySelector('.toggle-btn')
+        const togglebtnIcon = document.querySelector('.toggle-btn i')
+        const dropDownMenu= document.querySelector('.dropdown_menu')
+
+        togglebtn.onclick = function () {
+            dropDownMenu.classList.toggle('open')
+            const isOpen = dropDownMenu.classList.contains('open')
+
+            togglebtnIcon.classList = isOpen 
+            ? 'fa-solid fa-xmark'
+            : 'fa-solid fa-bars-staggered'
+        }
