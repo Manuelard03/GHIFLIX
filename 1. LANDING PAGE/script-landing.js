90a86d080afb34a1cb59b5aaca9857146f3942fa
Manuela <manuelarodriguezdavila@outlook.com>
2023-05-15 10:32:50 -0500

    avances

--- 1. LANDING PAGE/script-landing.js
+++ 1. LANDING PAGE/script-landing.js
@@ -93,3 +93,13 @@ const togglebtn = document.querySelector('.toggle-btn')
             e.preventDefault();
             modal.classList.remove('modal--show');
         });
+
+/*slider*/
+var counter = 1;
+        setInterval(function(){
+            document.getElementById('radio' + counter).checked = true;
+            counter++;
+            if(counter > 4){
+                counter = 1;
+            }
+        }, 5000)
